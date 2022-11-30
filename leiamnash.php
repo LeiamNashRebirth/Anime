1ff61a7835dec94d53d85e35a141c7561c114b24
Leiam Nash <94122613+LeiamNashRebirth@users.noreply.github.com>
2022-11-30 11:35:41 +0800

    Create leiamnash.php

---
+++ leiamnash.php
@@ -0,0 +1,28 @@
+<div id="bot">
+  <div id="container">
+    <div id="header">
+        Online Chatbot App
+    </div>
+
+    <div id="body">
+        <!-- This section will be dynamically inserted from JavaScript -->
+        <div class="userSection">
+          <div class="messages user-message">
+
+          </div>
+          <div class="seperator"></div>
+        </div>
+        <div class="botSection">
+          <div class="messages bot-reply">
+
+          </div>
+          <div class="seperator"></div>
+        </div>                
+    </div>
+
+    <div id="inputArea">
+      <input type="text" name="messages" id="userInput" placeholder="Please enter your message here" required>
+      <input type="submit" id="send" value="Send">
+    </div>
+  </div>
+  </div>
